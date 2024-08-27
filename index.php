595921ecc54620d60f56bbdb90d2f1ccd48511bb
fabert varico <fabertvaricodigital@gmail.com>
2024-08-27 16:07:10 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World1</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam minus nisi corrupti, ducimus recusandae corporis, omnis voluptates eaque ratione incidunt obcaecati. Nulla maxime illo neque accusantium placeat, asperiores obcaecati delectus!</P>
+    <ul>
+        <li>satu</li>
+        <li>satu</li>
+    <ul>
 </body>
 </html>
